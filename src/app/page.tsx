514760fdc47de792ff11794d15319db552826bce
KOMAKEOS <oliver.na.cormack@gmail.com>
2026-01-17 17:50:03 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2463,6 +2463,7 @@ const DesktopDetailPanel: React.FC = () => {
     Next →
   </button>
 </div>
+        
       </div>  {/* ← ADD: Close content div */}
     </div>    {/* ← ADD: Close main panel div */}
   )           {/* ← ADD: Close return statement */}
